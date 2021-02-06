2aa8a8d37245976f438ed5ea9e68d36969e299a1
Petro Husak <petro.husak.1998@gmail.com>
2021-02-06 11:58:06 +0200

    Updates

--- src/Image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Image(props){
-    return (
-        <li>
-            <img src={props.image.img_src} alt={props.image.id}/>
-        </li>
-    )
-}
